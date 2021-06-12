bfa73af6cd0dc72e90d4141d0cc38345414f7808
Mathias Latronico <mathiezelat@gmail.com>
2021-06-12 16:08:16 -0300

    MRTRICOACH - MTZ - v2.0 - NewNav

--- js/main.js
+++ js/main.js
@@ -1,26 +1,38 @@
-function openRes() {
-  document.querySelector(".navbar-nav").style.display="block";
-  document.querySelector(".closeMenuResponsive").style.display="block";
-  document.querySelector(".menuResponsive").style.display="none";
-  document.querySelector(".isotipo").style.display="none";
-  document.querySelector(".navbarLogo").style.display="none";  
-  document.querySelector(".navbar").style.flexDirection="column";
+const navSlide = ()=>{
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  burger.addEventListener('click', ()=>{
+    nav.classList.toggle('nav-active');
+    navLinks.forEach((link,index) =>{
+      if(link.style.animation){
+        link.style.animation = '';
+      } else{
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 4 + 0.5}s`;
+      }
+    });
+    burger.classList.toggle('toggle');
+  });
 }
-
-function closeRes(){
-  document.querySelector(".navbar-nav").style.display="none";
-  document.querySelector(".closeMenuResponsive").style.display="none";
-  document.querySelector(".menuResponsive").style.display="block";
-  document.querySelector(".isotipo").style.display="block";
-  document.querySelector(".navbarLogo").style.display="block";  
-  document.querySelector(".navbar").style.flexDirection="row";
+navSlide();
+const sections = document.querySelectorAll(".section");
+const verifyVisibility = (entries)=>{
+    for (const entry of entries) {
+        if(entry.isIntersecting){
+            console.log("se esta viendo: ", entry.target.id);
+            const menu = document.querySelector(`.${entry.target.id}`);
+            menu.style.color = "#26E573";
+          } else {
+            const menu = document.querySelector(`.${entry.target.id}`);
+            menu.style.color = "#FFFFFF";
+        }
+    }
 }
-
-function closeSide(){
-  document.querySelector(".navbar-nav").style.display="none";
-  document.querySelector(".closeMenuResponsive").style.display="none";
-  document.querySelector(".menuResponsive").style.display="block";
-  document.querySelector(".isotipo").style.display="block";
-  document.querySelector(".navbarLogo").style.display="block";  
-  document.querySelector(".navbar").style.flexDirection="row";
+const options = {
+    rootMargin: "-200px",
+    treshold: 0.1
+}
+const observer = new IntersectionObserver(verifyVisibility, options)
+for (const section of sections) {
+    observer.observe(section)
 }
