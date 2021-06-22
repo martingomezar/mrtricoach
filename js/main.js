b9b4b55199b9f65cddc62720c76b0c808a7e1915
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2021-06-22 02:00:20 -0300

    MRTRICOACH - MTZ - v2.2.3 - Menu Mejorado

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ const navSlide = ()=>{
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
-  const navLinksA = document.querySelectorAll('.nav-links li a');
-  navLinksA.addEventListener('click', ()=>{
+  nav.addEventListener('click', ()=>{
     nav.classList.remove('nav-active');
     navLinks.forEach((link,index) =>{
       if(link.style.animation){
